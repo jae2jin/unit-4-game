6d019488c6bd1bf005987c022997eb1a44e9d046
Michael Hoang <jae2jin@gmail.com>
2019-10-23 08:54:01 -0400

    Still can't fixed reset function in game.js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,13 @@ var colorButton = $('.colorButton');
 function startGame() {
     currentScore = 0;
     $('#score').text(currentScore);
-    $('#wins').text(winCounter);
-    $('#losses').text(lossCounter);
+    console.log("score: " + currentScore); 
 
+    $('#wins').text(winCounter);
+    console.log("wins: " + winCounter); 
+
+    $('#losses').text(lossCounter);
+	console.log("losses: " + lossCounter); 
 
     numberTarget = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
     console.log("AIM FOR THIS NUMBER ", numberTarget);
@@ -21,14 +25,25 @@ function startGame() {
         console.log(buttonValues);
     }
     $('#gem1').attr('value', buttonValues[0]);
+    console.log("gem1: " + buttonValues); 
 
     $('#gem2').attr('value', buttonValues[1]);
+    console.log("lossgem2es: " + buttonValues); 
 
     $('#gem3').attr('value', buttonValues[2]);
+    console.log("gem3: " + buttonValues); 
 
     $('#gem4').attr('value', buttonValues[3]);
+    console.log("gem4: " + buttonValues); 
 }
 startGame();
+
+// function reset () {
+//     numberTarget = Math.floor(Math.random() * (120 - 19 + 1)) + 19;
+//         console.log("numberTarget: " + numberTarget); 
+//     $(".randomNumber").onc(numberTarget); 
+// Can't freaking figure out how to automatically reset game or create a function that will reset once
+// the score has been match or lost the game once it been past.
 
 var playGame = function () {
     currentScore = currentScore + (Number($(this).attr('value')));
@@ -37,15 +52,18 @@ var playGame = function () {
         $('#score').text(currentScore)
         winCounter++;
         $('#wins').text(winCounter);
+        console.log("wins: " + winCounter); 
         reset();
     }
     else if (currentScore > numberTarget) {
         $('#score').text(currentScore);
         lossCounter++;
         $('#losses').text(lossCounter);
+        console.log("losses: " + lossCounter); 
         reset();
     }
     else {
     }
 }
 $('.colorButton').on('click', playGame);
+console.log("colorButton: " + playGame); 
